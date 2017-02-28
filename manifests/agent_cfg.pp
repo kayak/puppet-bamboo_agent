de66db9dec7cc04a20e51561f86be730b9aaa897
Cliff Wakefield <cliff.wakefield@gmail.com>
2017-02-28 12:02:09 +1100

    Fixed quotes

--- manifests/agent_cfg.pp
+++ manifests/agent_cfg.pp
@@ -11,12 +11,12 @@ define bamboo_agent::agent_cfg(
     owner => $bamboo_agent::user_name,
     group => $bamboo_agent::user_group,
   } ->
-  file_line { 'Update name field $path':
+  file_line { "Update name field $path":
     path    => $path,
     line    => "<name>$title</name>",
     match   => "^<name>.*?$"
   } ->
-  file_line { 'Update description field $path':
+  file_line { "Update description field $path":
   path    => $path,
   line    => "<description>$name</description>",
   match   => "^<description>.*?$"
